c40719a7d51f9f8d5dd1ad6e95ce9fb4e3bb4f4c
Capi Etheriel <barraponto@gmail.com>
2015-12-04 08:51:41 -0200

    fix configuration loading

--- blogful/__init__.py
+++ blogful/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 config_path = os.environ.get(
     'CONFIG_PATH', 'blogful.config.DevelopmentConfig')
-app.config_from_object(config_path)
+app.config.from_object(config_path)
 
 from . import views
 from . import filters
