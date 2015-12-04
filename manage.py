df8601b0db39aa3093722e0e093e63803107fd07
Capi Etheriel <barraponto@gmail.com>
2015-12-04 09:33:58 -0200

    setup seed command

--- manage.py
+++ manage.py
@@ -1,8 +1,11 @@
 import os
 
+import loremipsum
 from flask.ext.script import Manager
 
 from blogful import app
+from blogful.database import session
+from blogful.models import Entry
 
 manager = Manager(app)
 
@@ -10,6 +13,15 @@ manager = Manager(app)
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+
+@manager.command
+def seed(entries=10):
+    for n in entries:
+        session.add(Entry(
+            title='Test Entry #{}'.format(n),
+            content=loremipsum.get_paragraph(start_with_lorem=True)
+        ))
+    session.commit()
 
 if __name__ == '__main__':
     manager.run()
