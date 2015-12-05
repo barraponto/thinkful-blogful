859d752cf6f35d9251319887066867a444d1968d
Capi Etheriel <barraponto@gmail.com>
2015-12-04 22:58:02 -0200

    displaying entries

---
+++ blogful/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+
+from . import app
+from .models import Entry
+
+
+@app.route('/')
+def entries():
+    entries = Entry.query.order_by(Entry.datetime.desc()).all()
+    return render_template('entries.html', entries=entries)
