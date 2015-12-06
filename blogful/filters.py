bb674d1c6867de362f772da26a8359ca586f10ea
Capi Etheriel <barraponto@gmail.com>
2015-12-06 11:50:58 -0200

    added entries markup and filters

---
+++ blogful/filters.py
@@ -0,0 +1,13 @@
+import mistune
+from flask import Markup
+
+from . import app
+
+
+@app.template_filter()
+def markdown(text):
+    return Markup(mistune.markdown(text, escape=True))
+
+@app.template_filter()
+def dateformat(date, format):
+    return date.strftime(format)
