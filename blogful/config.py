c914aec6e4418a418d3db86652d6a450ff0bfc37
Capi Etheriel <barraponto@gmail.com>
2015-12-13 22:56:49 -0200

    setup testing

--- blogful/config.py
+++ blogful/config.py
@@ -1,3 +1,8 @@
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful-tests'
+    DEBUG = False
+    SECRET_KEY = 'this-is-just-for-testing'
+
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful'
     DEBUG = True
