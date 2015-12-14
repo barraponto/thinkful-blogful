ca8a3cf4f754715aab65fb944ee822dd540326c7
Capi Etheriel <barraponto@gmail.com>
2015-12-14 17:39:48 -0200

    silence flask-sqlalchemy warnings

--- blogful/config.py
+++ blogful/config.py
@@ -1,9 +1,12 @@
-class TestingConfig(object):
+class BaseConfig(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+class TestingConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful-tests'
     DEBUG = False
     SECRET_KEY = 'this-is-just-for-testing'
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful'
     DEBUG = True
     SECRET_KEY = 'this-is-just-for-development'
