c816f3c5cbc9ece8a68fbb84c0c920a0097a76d5
Capi Etheriel <barraponto@gmail.com>
2015-12-16 22:27:33 -0200

    disable CSRF protection during tests

--- blogful/config.py
+++ blogful/config.py
@@ -5,6 +5,7 @@ class TestingConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful-tests'
     DEBUG = False
     SECRET_KEY = 'this-is-just-for-testing'
+    WTF_CSRF_ENABLED = False
 
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful'
