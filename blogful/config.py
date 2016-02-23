2c66bf90b270907a98751907970dbaaf1c5a7dbf
Capi Etheriel <barraponto@gmail.com>
2016-02-23 09:33:59 -0300

    pep8 to trigger travis

--- blogful/config.py
+++ blogful/config.py
@@ -1,16 +1,19 @@
 class BaseConfig(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
+
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful'
     DEBUG = True
     SECRET_KEY = 'this-is-just-for-development'
+
 
 class TestingConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost/tf-blogful-tests'
     DEBUG = False
     SECRET_KEY = 'this-is-just-for-testing'
     WTF_CSRF_ENABLED = False
+
 
 class TravisConfig(TestingConfig):
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost:5432/tf-blogful-tests'
